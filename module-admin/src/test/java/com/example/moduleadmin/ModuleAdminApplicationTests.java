838d23a17deb3a4055a381283ed0f1d0e9e40d49
minoflower31@naver.com <minoflower31@naver.com>
2023-07-20 18:51:24 +0900

    feat: admin, api, core 모듈 추가

---
+++ module-admin/src/test/java/com/example/moduleadmin/ModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduleadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
